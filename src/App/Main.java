4f8b4ed3145aedc6a09cca12005548c7d32b54bb
SAINTPHARD <pierreobed67@gmail.com>
2025-09-25 13:47:58 -0300

    Classe de entidade de departamento

---
+++ src/App/Main.java
@@ -0,0 +1,15 @@
+package App;
+
+import model.entities.Departement;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Departement d = new Departement(1, "Pen");
+		System.out.println(d);
+		
+		
+	}
+
+}
